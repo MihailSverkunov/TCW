c4c19b2c94c61b34925f1bb8d626f30fc721f4ef
Михаил <Mihail.sverkynov@gmail.com>
2017-11-22 15:42:45 +0700

    qwe

--- Task.Manager.DataAccess.UnitOfWork/IAsyncQueryable.cs
+++ Task.Manager.DataAccess.UnitOfWork/IAsyncQueryable.cs
@@ -11,5 +11,45 @@ namespace TaskManagerSM.DataAccess.UnitOfWork
         Task<TSource> FirstOrDefaultAsync();
 
         Task<TSource> FirstOrDefaultAsync(Expression<Func<TSource, bool>> predicate);
+
+        Task<bool> AllAsync(Expression<Func<TSource, bool>> predicate);
+
+        Task<int> CountAsync();
+
+        Task<int> CountAsync(Expression<Func<TSource, bool>> predicate);
+
+        Task<TSource> FirstAsync();
+
+        Task<TSource> FirstAsync(Expression<Func<TSource, bool>> predicate);
+
+        Task<TSource> LastAsync();
+
+        Task<TSource> LastAsync(Expression<Func<TSource, bool>> predicate);
+
+        Task<TSource> LastOrDefaultAsync();
+
+        Task<TSource> LastOrDefaultAsync(Expression<Func<TSource, bool>> predicate);
+
+        Task<TSource> SingleAsync();
+
+        Task<TSource> SingleAsync(Expression<Func<TSource, bool>> predicate);
+
+        Task<TSource> SingleOrDefaultAsync();
+
+        Task<TSource> SingleOrDefaultAsync(Expression<Func<TSource, bool>> predicate);
+
+        Task<TSource> MinAsync();
+
+        // Task<TResult> MinAsync<T, TResult>(Expression<Func<T, TResult>> selector);
+
+        Task<TSource> MaxAsync();
+
+        //Task<TResult> MaxAsync<TSource, TResult>(Expression<Func<TSource, TResult>> selector)
+
+        
+
+        Task<List<TSource>> ToListAsync();
+
+        Task<TSource[]> ToArrayAsync();
     }
 }
--- TaskMnanagerSM.DataAccess.UnitOfWork.Implementation/AsyncQueryable.cs
+++ TaskMnanagerSM.DataAccess.UnitOfWork.Implementation/AsyncQueryable.cs
@@ -1,5 +1,6 @@
 ﻿
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Extensions.Internal;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -30,6 +31,110 @@ namespace TaskMnanagerSM.DataAccess.UnitOfWork.Implementation
             return _query.FirstOrDefaultAsync(predicate);
         }
 
+
+        public Task<bool> AllAsync(Expression<Func<T, bool>> predicate)
+        {
+
+            return _query.AllAsync(predicate);
+        }
+
+        public Task<int> CountAsync()
+        {
+            return _query.CountAsync();
+        }
+
+        public Task<int> CountAsync(Expression<Func<T, bool>> predicate)
+        {
+            return _query.CountAsync(predicate);
+        }
+
+        public Task<T> FirstAsync()
+        {
+            return _query.FirstAsync();
+        }
+
+        public Task<T> FirstAsync(Expression<Func<T, bool>> predicate)
+        {
+
+            return _query.FirstAsync(predicate);
+        }
+
+        public Task<T> LastAsync()
+        {
+            return _query.LastAsync();
+        }
+
+        public Task<T> LastAsync(Expression<Func<T, bool>> predicate)
+        {
+            return _query.LastAsync(predicate);
+        }
+
+        public Task<T> LastOrDefaultAsync()
+        {
+            return _query.LastOrDefaultAsync();
+        }
+
+        public  Task<T> LastOrDefaultAsync(Expression<Func<T, bool>> predicate)
+        {
+            return _query.LastOrDefaultAsync(predicate);
+        }
+
+        public Task<T> SingleAsync()
+        {
+            return _query.SingleAsync();
+        }
+
+        public Task<T> SingleAsync(Expression<Func<T, bool>> predicate)
+        {
+            return _query.SingleAsync(predicate);
+        }
+
+        public Task<T> SingleOrDefaultAsync()
+        {
+            return _query.SingleOrDefaultAsync();
+        }
+
+        public Task<T> SingleOrDefaultAsync(Expression<Func<T, bool>> predicate)
+        {
+            return _query.SingleOrDefaultAsync(predicate);
+        }
+
+        public Task<T> MinAsync()
+        {
+            return _query.MinAsync();
+        }
+
+        //public Task<TResult> MinAsync<T, TResult>(Expression<Func<T, TResult>> selector)
+        //{
+        //    return _query.MinAsync(_maxSelector, selector);
+        //}
+
+        public Task<T> MaxAsync()
+        {
+            return _query.MaxAsync();
+        }
+
+        //public static Task<TResult> MaxAsync<TSource, TResult>(Expression<Func<TSource, TResult>> selector)
+        //{
+        //    return ExecuteAsync<TSource, TResult>(_maxSelector, source, selector, cancellationToken);
+        //}
+
+        //public Task<decimal> SumAsync()
+        //{
+        //    return _query.SumAsync();
+        //}
+
+        //public Task<bool> ContainsAsync<TSource>(TSource item)
+        //{
+        //    return _query.ContainsAsync(
+        //        Expression.Constant(item, typeof(TSource)));
+        //}
+
+        public Task<List<T>> ToListAsync()
+            => _query.AsAsyncEnumerable().ToList();
+
+        public Task<T[]> ToArrayAsync()
+           => _query.AsAsyncEnumerable().ToArray();
 
     }
 }
